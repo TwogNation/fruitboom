7dbd3dbae6f5adfae7d8ae8287c30a3b1e13e576
Matheus N <matheusnaschaves@gmail.com>
2022-07-07 16:49:46 +0200

    new game

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,20 @@
+var postMsg = ''
+
+function postText(value){
+	console.log('iFrame sender: ' + value) 
+	parent.postMessage(value, "*")
+}
+
+
+
+const scriptsInEvents = {
+
+		async Es_result_Event1_Act8(runtime, localVars)
+		{
+			postText(runtime.globalVars.postMsg)
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
